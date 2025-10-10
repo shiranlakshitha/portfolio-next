6c1b771e01e6d9dcc94b101dd92f146493e1fd9d
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-10-10 17:12:35 +0530

    feat: add DbScroll component for technology stack display and enhance Intro layout

---
+++ src/components/DbScroll.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "../app/globals.css";
+
+const techStack = [
+  "ReactJS",
+  "VueJS",
+  "NextJS",
+  "NuxtJS",
+  "Express",
+  "NestJS",
+  "TypeScript",
+  "GraphQL",
+  "NodeJS",
+  "MongoDB",
+  "PostgreSQL",
+  "Docker",
+];
+
+const DbScroll = () => {
+  return (
+    <div className="hide-scrollbar w-full h-64 overflow-hidden rounded-2xl p-4 relative">
+      <div className="animate-scroll-down flex flex-col space-y-4">
+        {/* Duplicate the list for seamless infinite scroll */}
+        {[...techStack, ...techStack].map((tech, index) => (
+          <div
+            key={index}
+            className="bg-[#0d0f1a] text-white px-6 py-3 rounded-xl border border-gray-700 hover:border-indigo-500 hover:bg-[#1a1d2f] transition-all duration-300 flex-shrink-0"
+          >
+            {tech}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DbScroll;
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,22 +5,24 @@ import WorldMap from '../assets/section input.png'
 import TechScroll from '../components/TechScroll'
 import Pointer1 from '../assets/Group.png'
 import Pointer2 from '../assets/Button.png'
+import DbScroll from './DbScroll'
 
 const Intro = () => {
   return (
-    <div className='px-[50px] my-[50px]'>
+    <div className='px-[75px] my-[50px]'>
         <div className='flex flex-row h-[540px] gap-[25px] font-inter'>
             <div className='flex-1 h-[540px] relative w-full rounded-3xl'>
                 <Image src={IntroImg} className='object-cover rounded-3xl h-full w-full' alt='intro-img' />
-                <Image className='absolute right-5 bottom-[75px]' src={Pointer1} alt='pointer-1' />
-                <Image src={Pointer2} className='absolute left-5 top-[75px]' alt='pointer-2' />
+                <Image className='absolute right-10 bottom-[75px]' src={Pointer1} alt='pointer-1' />
+                <Image src={Pointer2} className='absolute left-10 top-[75px]' alt='pointer-2' />
                 <h4 className='absolute bottom-5 left-5 font-bold text-white w-[400px] text-[35px]'>
                     I prioritize client collaboration, fostering open communication 
                 </h4>
             </div>
-            <div className='flex flex-1 gap-[10px] flex-col'>
-                <div className='bg-gradient-to-tl from-white/20 to-transparent flex-1 w-full h-[20vh] rounded-3xl border-1 border-[#363749]'>
+            <div className='flex flex-1 gap-[25px] flex-col'>
+                <div className='relative bg-gradient-to-tl from-white/10 to-transparent flex-1 w-full h-[20vh] rounded-3xl border-1 border-[#363749]'>
                     <Image className='object-cover rounded-3xl w-full h-auto' src={WorldMap} alt='world-img' />
+                    <h4 className='absolute top-10 left-10 text-white text-[30px] font-bold'>Remote-Ready Developer</h4>
                 </div>
                 <div className='flex flex-1 flex-col relative px-[15px] bg-gradient-to-b from-[#04071D] to-[#0C0E23] rounded-3xl border-1 border-[#363749]'>
                     <div className='absolute top-[100px] left-5 text-white'>
@@ -34,7 +36,9 @@ const Intro = () => {
                         <div className='absolute top-0 right-8 h-full overflow-hidden'>
                             <TechScroll />
                         </div>
-                        <div></div>
+                        <div className='absolute top-0 right-[200px] h-full overflow-hidden'>
+                            <DbScroll />
+                        </div>
                     </div>
                 </div>
             </div>
