884d060a2fdba71d3b4108ac9cf6f91db3f978b7
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-10-17 19:08:45 +0530

    feat: implement Project and Timeline schemas with required fields for mongoose integration

---
+++ src/models/Project.ts
@@ -0,0 +1,23 @@
+import { model, models, Schema } from "mongoose";
+
+
+const ProjectSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: false
+    },
+    image: {
+        type: String,
+        required: false
+    },
+    description: {
+        type: String,
+        required: true
+    }
+})
+
+export default models.ProjectSchema || model("Project", ProjectSchema)
--- src/models/Timeline.ts
+++ src/models/Timeline.ts
@@ -2,10 +2,22 @@ import { model, models, Schema } from "mongoose";
 import { string } from "three/tsl";
 
 const TimeLineSchema = new Schema({
-    title: string,
-    year: string,
-    description: string,
-    category: string
+    title: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    }
 })
 
 export default models.TimeLine || model("Timeline", TimeLineSchema)
