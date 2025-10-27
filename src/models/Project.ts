3aba1d46977c0f1ab3c734c54e23178fca519fe4
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-10-27 23:10:04 +0530

    feat: add techStack field to Project schema for enhanced project details

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -14,6 +14,10 @@ const ProjectSchema = new Schema({
         type: String,
         required: false
     },
+    techStack : [{
+        type: String,
+        required: true
+    }],
     description: {
         type: String,
         required: true
